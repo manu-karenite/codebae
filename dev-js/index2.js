0cb706120b3b2da90d1ecde7487524928d0b2058
Manavesh Narendra <manavesh.narendra@gmail.com>
2021-09-14 02:51:04 +0530

    Created Message and changed the colors

--- dev-js/index2.js
+++ dev-js/index2.js
@@ -9,7 +9,7 @@ const upcomingHead=`      <tr class="individual-contest">
                        <td class="start-dateh icsoloh">Starts</td>
                        <td class="end-dateh icsoloh">Ends</td>
                        <td class="registerh icsoloh">Register</td>
-                       <td class="durationh icsoloh">in 1 Day</td>
+                       <td class="durationh icsoloh">Status</td>
                     </tr>`;
 
 
@@ -36,18 +36,19 @@ function upcomingDisplay(upcomingContests)
     }  
     let [startDate,endDate] = [new Intl.DateTimeFormat("en-IN",option).format(new Date(item.start_time)),new Intl.DateTimeFormat('en-IN',option).format(new Date(item.end_time))];
 
-    const classToPut="greenc";
-    if(item.in_24_hours==="NO")
-        classToPut="redc";
+    let classToPut="redc";
+    let message="1d remaining!"
+    if(item.in_24_hours=="No")
+        {classToPut="greenc";message=">1d remaining"}
         html = `
                 <tr class="individual-contest">
-                    <td class="name icsolo">${item.name}</td>
+                        <td class="name icsolo">${item.name}</td>
                        <td class="platform icsolo">${item.site}</td>
                        <td class="start-date icsolo">${startDate}</td>
                        <td class="end-date icsolo">${endDate}</td>
                        <td class="register icsolo">
                        <a href="${item.url}" target="_blank" class="remove-decoration">Register</a></td>
-                        <td class=in24Hours icsolo ${classToPut}">yes</td>
+                        <td class="in24Hours icsolo ${classToPut}">${message}</td>
                        
                   </tr>`;
         upcomingSection.insertAdjacentHTML("beforeend",html);
