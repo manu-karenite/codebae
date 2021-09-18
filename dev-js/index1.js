4b584ffb8d66aa379d5878a215883e0dc45ce36a
Manavesh Narendra <manavesh.narendra@gmail.com>
2021-09-18 23:11:14 +0530

    Removed some contents and shifted to common.js

--- dev-js/index1.js
+++ dev-js/index1.js
@@ -1,5 +1,5 @@
 'use strict';
-const wishLabel = document.querySelector(".wish");
+
 const movetoContestsButton = document.querySelector('.movetoSites');
 const sectionPlatforms = document.querySelector('.platforms');
 const runningSection = document.querySelector('.running-contests');
@@ -13,23 +13,7 @@ const runningHead=`      <tr class="individual-contest">
                        <td class="durationh icsoloh">Status</td>
                     </tr>`;
 ///////////////////////////////////////////
-const now = new Date();
-console.log(now);
-const hour = now.getHours();
-console.log(hour);
-let mood =undefined;
 
-
-if(hour>=6 && hour<=11)
-    mood="Morning";
-else if(hour>=12 && hour<=16)
-    mood ="Afternoon";
-else if(hour>=17 && hour<=21)
-    mood ="Evening";
-else
-    mood="Night";
-
-wishLabel.textContent=`Good ${mood}!`;
 
 /////////////////////////////////
 movetoContestsButton.addEventListener('click',(e)=>
