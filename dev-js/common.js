554247626705d899c209cab75f695fc49d612959
Manavesh Narendra <manavesh.narendra@gmail.com>
2021-09-18 23:03:24 +0530

    Adde a common JS file to contain common components

---
+++ dev-js/common.js
@@ -0,0 +1,19 @@
+'use strict';
+const wishLabel = document.querySelector(".wish");
+const now = new Date();
+console.log(now);
+const hour = now.getHours();
+console.log(hour);
+let mood =undefined;
+
+
+if(hour>=6 && hour<=11)
+    mood="Morning";
+else if(hour>=12 && hour<=16)
+    mood ="Afternoon";
+else if(hour>=17 && hour<=21)
+    mood ="Evening";
+else
+    mood="Night";
+
+wishLabel.textContent=`Good ${mood}!`;
