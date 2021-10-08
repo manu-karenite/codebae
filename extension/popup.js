df43d9a92b247de2ac02e9d166ffa39123142787
Manavesh Narendra <manavesh.narendra@gmail.com>
2021-10-08 16:49:39 +0530

    Added color combination in JSonly

--- extension/popup.js
+++ extension/popup.js
@@ -16,8 +16,6 @@ const option =
         hour :"numeric",
         minute:"numeric",
     }  
-// console.log(dividerWrapperOngoing);
-// console.log(dividerWrapperUpcoming)
 btnUpcoming.addEventListener("click",function(e)
 {
     e.preventDefault();
@@ -32,10 +30,7 @@ btnUpcoming.addEventListener("click",function(e)
     window.setTimeout(()=>
         {
             divider.style.backgroundColor="#fff";
-           // dividerWrapperOngoing.innerHTML="";
            document.querySelector("#centerGIF").style.display="none";
-
-            //non threading in nature, so it is fine! It would not block the thread pool
         },
         4000
     )
@@ -44,45 +39,35 @@ btnUpcoming.addEventListener("click",function(e)
         return response.json();
     })
     .then(function (data) {
-       // console.log(data);
         const upcomingContests = data.filter(function (element, index) {
-            // console.log(element);
-            // const end_time = new Date(element.end_time);
             
-            // end_time = new Intl.DateTimeFormat('en-GB').format(end_time);
-            // const today = new Intl.DateTimeFormat('en-GB').format(new Date());
-            // console.log(end_time,today);
-            // return (end_time > today);
-            //console.log(element);
-            //const end_time = new Date(element.end_time);
-            //end_time = new Intl.DateTimeFormat('en-GB').format(end_time);
-            //console.log(new Intl.DateTimeFormat('en-GB').format(new Date(element.end_time)),new Intl.DateTimeFormat('en-GB').format(new Date()));
-            
-            //return  (new Intl.DateTimeFormat('en-GB').format(new Date(element.start_time))>new Intl.DateTimeFormat('en-GB').format(new Date())) && element.status==='BEFORE' ;
             return (new Date(element.start_time).getTime() > new Date().getTime());
 
         });
-        //upcomingContests.reverse();
          console.log(upcomingContests);
         console.log("promise fetched and data restored");
         buildProgram1(upcomingContests);
-        //buildProgram(upcomingContests);
     });
     function buildProgram1(upcomingContests)
     {
         divider.style.padding="0rem 0rem";
         for(let i=0;i<upcomingContests.length;i++)
         {
-        //console.log(new Date(upcomingContests[i].end_time));
+        let bg = "#062743";
+        let color = "#f5f5f5";
+        if (i % 2 == 1) {
+            bg = "#c4ffdd";
+            color = "#000";
+        }
        const endDate= (new Intl.DateTimeFormat(navigator.language,option).format(new Date(upcomingContests[i].end_time)));
        const startDate= (new Intl.DateTimeFormat(navigator.language,option).format(new Date(upcomingContests[i].start_time)));
         const templateHTML=`
-        <div class="contestUpcoming">
-            <div class="name">${upcomingContests[i].name}</div>
-            <div class="starts"><b>Starts:</b> ${startDate}</div>
-            <div class="platform">${upcomingContests[i].site}</div>
-            <div class="ends"><b>Ends:</b> ${endDate}</div>
-             <div class="register"><a href="${upcomingContests[i].url}" target="_blank">Join</a></div>  
+        <div class="contestUpcoming" style="background-color:${bg}">
+            <div class="name" style="color:${color}">${upcomingContests[i].name}</div>
+            <div class="starts" style="color:${color}"><b>Starts:</b> ${startDate}</div>
+            <div class="platform" style="color:${color}">${upcomingContests[i].site}</div>
+            <div class="ends" style="color:${color}"><b>Ends:</b> ${endDate}</div>
+             <div class="register" ><a href="${upcomingContests[i].url}" target="_blank" style="color:${color}">Register</a></div>  
         </div>
         `;
            dividerWrapperUpcoming.insertAdjacentHTML("beforeend",templateHTML);
@@ -97,13 +82,10 @@ btnUpcoming.addEventListener("click",function(e)
 btnOngoing.addEventListener("click",function(e)
 {
     e.preventDefault();
-    // dividerWrapperUpcoming.innerHTML="";
-    // dividerWrapperOngoing.innerHTML="";
+
     const htmlToPut=`
-    <div id="centerGIF"><center><img src="../img/wait.gif"></center></div>
+    <div id="centerGIF"><center><img src="../img/wait2.gif"></center></div>
     `;
-    // dividerWrapperUpcoming.insertAdjacentHTML("beforeend",htmlToPut);
-    // dividerWrapperOngoing.insertAdjacentHTML("beforeend",htmlToPut);
     console.log("hello");
     //step1: empty the upcoming tag or simply remove it
     dividerWrapperUpcoming.style.display="none";
@@ -116,10 +98,8 @@ btnOngoing.addEventListener("click",function(e)
     window.setTimeout(()=>
         {
             divider.style.backgroundColor="#fff";
-           // dividerWrapperOngoing.innerHTML="";
            document.querySelector("#centerGIF").style.display="none";
 
-            //non threading in nature, so it is fine! It would not block the thread pool
         },
         4000
     )
@@ -142,21 +122,25 @@ btnOngoing.addEventListener("click",function(e)
         divider.style.padding="0rem 0rem";
         for(let i=0;i<runningContests.length;i++)
         {
-        //console.log(new Date(runningContests[i].end_time));
+
+        let bg = "#302a77";
+        let color = "#f5f5f5";
+        if (i % 2 == 1) {
+            bg = "#fdea2e";
+            color = "#000";
+        }
        const endDate= (new Intl.DateTimeFormat(navigator.language,option).format(new Date(runningContests[i].end_time)));
         const templateHTML=`
-        <div class="contestOngoing">
-            <div class="name">${runningContests[i].name}</div>
+        <div class="contestOngoing"  style="background-color:${bg}">
+            <div class="name" style="color:${color}">${runningContests[i].name}</div>
             <div class="ends-platform">
-            <div class="ends"><b>Ends:</b> ${endDate}</div>
-            <div class="platform">${runningContests[i].site}</div>
+            <div class="ends" style="color:${color}"><b>Ends:</b> ${endDate}</div>
+            <div class="platform" style="color:${color}">${runningContests[i].site}</div>
             </div>
-            <div class="register"><a href="${runningContests[i].url}" target="_blank">Join</a></div>  
+            <div class="register"><a href="${runningContests[i].url}" target="_blank" style="color:${color}">Join</a></div>  
         </div>
         `;
            dividerWrapperOngoing.insertAdjacentHTML("beforeend",templateHTML);
         }
-
-
     }
 })
